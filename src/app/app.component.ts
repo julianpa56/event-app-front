438c9f6461f0590aeb5bc4a917d791ab66938517
Julian Peña <julianpa56@gmail.com>
2024-02-15 21:39:17 -0300

    Inclusion de Tailwind y Preline, y creacion de organizacion de carpetas principales

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,68 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet, Event, NavigationEnd, RouterLink } from '@angular/router';
+import { IStaticMethods } from 'preline/preline';
+import { FormsModule } from '@angular/forms';
+declare global {
+  interface Window {
+    HSStaticMethods: IStaticMethods;
+  }
+}
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, RouterLink, FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'event-app';
+
+  selectedMonth: number = 6; // July
+  selectedYear: number = 2023;
+  months: string[] = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+  years: number[] = [2023, 2024, 2025, 2026, 2027];
+
+  title = 'Event app';
+
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.router.events.subscribe((event: Event) => {
+      if (event instanceof NavigationEnd) {
+        setTimeout(() => {
+          // @ts-ignore
+          HSStaticMethods.autoInit();
+        }, 100);
+      }
+    });
+  }
+
+  prevMonth() {
+    if (this.selectedMonth === 0) {
+      this.selectedMonth = 11; // December
+      this.selectedYear--;
+    } else {
+      this.selectedMonth--;
+    }
+  }
+
+  nextMonth() {
+    if (this.selectedMonth === 11) {
+      this.selectedMonth = 0; // January
+      this.selectedYear++;
+    } else {
+      this.selectedMonth++;
+    }
+  }
+
+  applySelection() {
+    console.log('Selected Month:', this.months[this.selectedMonth]);
+    console.log('Selected Year:', this.selectedYear);
+    // Aquí puedes implementar la lógica para aplicar la selección de fecha
+  }
+
+  cancelSelection() {
+    // Aquí puedes implementar la lógica para cancelar la selección de fecha
+  }
 }
