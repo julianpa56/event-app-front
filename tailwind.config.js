72cb0bda3245e680962c887349e68aff1d6f5814
Julian Peña <julianpa56@gmail.com>
2024-02-21 00:05:49 -0300

    Cambio de preline a Flowbite como libreria de componentes de Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,20 +2,12 @@
 module.exports = {
   content: [
     "./src/**/*.{html,ts}",
-    './node_modules/preline/preline.js',
+    "./node_modules/flowbite/**/*.js",
   ],
   theme: {
-    extend: {
-      fontFamily: {
-        sans: [
-          'Inter, ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Helvetica Neue, Arial, Noto Sans, sans-serif, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol, Noto Color Emoji',
-        ],
-      },
-    },
   },
   plugins: [
-    require('preline/plugin')
-  ],
-  darkMode: 'class',
+    require('flowbite/plugin'),
+  ]
 }
 
